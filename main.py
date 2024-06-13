3b9708daf19ead0710e32d512cbf09f9b510f055
shivani kasaudhan <kasaudhnshivani03@gmail.com>
2024-06-13 18:38:36 +0530

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker 2.0 created by Shivani")
+    while True:
+        x = input("Enter what you want to pronounce: ")
+        if x.lower() == "quit":
+            bye_command = 'powershell -Command "Add-Type -AssemblyName System.Speech; ' \
+                          '$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; ' \
+                          '$speak.Speak(\'bye bye friend\');"'
+            os.system(bye_command)
+            break
+
+        command = f'powershell -Command "Add-Type -AssemblyName System.Speech; ' \
+                  f'$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; ' \
+                  f'$speak.Speak(\'{x}\');"'
+        os.system(command)
